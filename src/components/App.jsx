c5084c4b9576f276c21c07746abac46d632d05e6
nikhilbatta <nbatta08@gmail.com>
2019-11-15 10:40:17 -0800

    app renders without crashing, eslint throws too many errors ughhh

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { Switch, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+function App(){
+  
+  return (
+    <div>
+      
+      <Switch>
+        <h2>Hello World</h2>
+        <Route></Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
