ad31647ec49f612d190476c71bba32c9a54946c2
nikhilbatta <nbatta08@gmail.com>
2019-11-15 16:51:03 -0800

    adds styling for form for new keg

--- src/components/Newkeg.jsx
+++ src/components/Newkeg.jsx
@@ -2,22 +2,30 @@ import React from 'react';
 
 function Newkeg(){
     return (
-        <div>
+        <div class="container">
         <form>
+            <div class="form-group">
           <input
             type='text'
             id='name'
             placeholder='Name of the keg'/>
+            </div>
+            <div class="form-group">
           <input
             type='text'
             id='brand'
             placeholder='Brand of the keg'/>
+            </div>
+            <div class="form-group">
           <input
             id='price'
             placeholder='Price per pint.'/>
+            </div>
+            <div class="form-group">
             <input
              id='alcContent'
              placeholder='alcohol content for the keg'/>
+             </div>
           <button type='submit'>Help!</button>
         </form>
       </div>
